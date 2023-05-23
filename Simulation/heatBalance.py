3e3849cbc092c4bcafa975e7b50f232ebb452829
Jennifer Weber <jennifer.weber@t-online.de>
2023-05-23 17:29:43 +0200

    added cp and density

--- Simulation/heatBalance.py
+++ Simulation/heatBalance.py
@@ -58,3 +58,53 @@ def visualizeIsBoundary(isBoundary):
     plt.savefig(f'{dir_path}/isBoundary.png')
 
 visualizeIsBoundary(isBoundary)
+
+temperatureArray = np.ones((discretization[0],discretization[1])) * 270
+temperatureArray[coalTopPosition:coalBottomPosition,:] = 1070
+temperatureArray[soilTopPosition:soilBottomPosition,:] = 270
+for i in range(discretization[0]):
+    for j in range(discretization[1]):
+        d = ((j - stoneCenterOfGravity[1])**2 + (i - stoneCenterOfGravity[0])**2)**0.5
+        if d <=  int((stoneDiameter/2)/meshSize):
+            temperatureArray[i,j] = 390
+
+
+def visualizeTemperatureField(temperatureArray):
+    # cmap = LinearSegmentedColormap.from_list('colorMaptempArray', N= 4)
+    plt.imshow(temperatureArray, cmap='hot')
+    plt.savefig(f'{dir_path}/temperatureArrayt')
+
+visualizeTemperatureField(temperatureArray)
+
+t = 0
+tmax = 100
+dT = 0.001
+iteration = 0
+picNumber = 1
+for i in range(tmax):
+    t = t + dT
+    iteration += 1
+    if iteration % 100 == 0:
+        picNumber += 1
+        plt.imshow(temperatureArray, cmap='hot')
+        plt.savefig(f'{dir_path}/temperatureArray{picNumber}')
+
+
+cpDict = {0: 1, 1: 1.02, 2: 0.8, 3: 1.005} #in kJ kg-1 K-1  ; urch Erhöhung der Pyrolyseendtemperatur von 400 auf 1200 °C steigt die spezifische Wärme der H. von 1,02 auf 1,60 kJ/kg K an
+densityDict = {0: 2.5, 1: 0.25, 2: 0.92, 3: 0.783 } # in g cm-3 
+thermalConductivityDict = {0: 2.0, 3: 0.02} #in J s-1 m-1 K-1
+
+def getcp(i,j):
+    oa = objectAssignment[i,j]
+    cp = cpDict[oa]
+    return cp
+
+def getDesity(i,j):
+    oa = objectAssignment[i,j]
+    density = densityDict[oa]
+    return density
+
+def getMass(i,j):
+    volumen = meshSize**2
+    mass = getDesity(i,j) * volumen
+    return mass
