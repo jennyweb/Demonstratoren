5117b56aa8d21f87daa146cbc6150f3f3ffd1569
Jennifer Weber <jennifer.weber@t-online.de>
2023-06-13 15:28:59 +0200

    added fit equation

--- fitEquationToData/csvRead.py
+++ fitEquationToData/csvRead.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_excel('fitEquationToData\sizeDistribution.xlsx', header=1)
-df.plot(x=df['Größe (μm)'], y=df['% Volumen In'],  kind='scatter')
+size = []
+volume = []
+for i in range(len(df['Größe (μm)'])):
+    size.append(df['Größe (μm)'][i])
+    volume.append(df['% Volumen In'][i])
+
+
+
+sigma = 0.2
+µ = 40
+p = []
+for i in range(len(size)):
+    x = (1/(sigma * size[i] * (2* np.pi)**0.5)) * np.exp(-((np.log(size[i])-µ)**2)/(2*sigma**2))
+    p.append(x)
+    print(max(p))
+
+plt.plot(size, volume, 'b')
+plt.plot(size, p, 'g')
 plt.show()
