1b67da48e9a2cd3d229544c870efabf817664f49
Jennifer Weber <jennifer.weber@t-online.de>
2023-06-13 10:06:50 +0200

    added visualization

--- divingComputer/tauchprofil.py
+++ divingComputer/tauchprofil.py
@@ -1,15 +1,41 @@
-with open('DivingComputer\divingprofile.txt', 'w') as fout:
-    fout.write('Zeit\tTiefe\n')
-    aktuelleTiefe = 0
-    for i in range(200):
-        aktuelleZeit = i * 10
-        if aktuelleTiefe < 18:
-            if aktuelleZeit % 4 == 0:
-                aktuelleTiefe = aktuelleTiefe
-            else:
-                aktuelleTiefe = i * 0.3
-            fout.write(f'{aktuelleZeit}\t{aktuelleTiefe}\n')
-        else: 
-            aktuelleTiefe = 18
-            fout.write(f'{aktuelleZeit}\t{aktuelleTiefe}\n')
+import csv
+import matplotlib.pyplot as plt
+
+def getDiveProfile(depth): 
+    currentDepth = 0
+    filename = f'divingprofile_{depth}'
+    with open(f'DivingComputer\{filename}.txt', 'w') as fout:
+        fout.write('time\tdepth\n')
+        for i in range(240):
+            currentTime = i * 10
+            if currentDepth > depth:
+                if currentTime % 4 == 0:
+                    currentDepth = currentDepth
+                else:
+                    currentDepth -= i * 0.3
+                fout.write(f'{currentTime}\t{currentDepth}\n')
+            else: 
+                currentDepth = depth
+                fout.write(f'{currentTime}\t{currentDepth}\n')
+    time = []
+    depth = []
+    with open(f'DivingComputer\{filename}.txt', 'r') as fin:
+        for line in csv.DictReader(fin, delimiter='\t'):
+            time.append(line['time'])
+            depth.append(float(line['depth']))
+    visualizeDivingProfile(time,depth, filename)
+    
+def visualizeDivingProfile(time,depth,filename):    
+    plt.plot(time, depth)
+    plt.title(filename)
+    plt.xlabel('time')
+    plt.ylabel('depth')
+    plt.savefig(f'DivingComputer\{filename}.png')
+
+
+getDiveProfile(-13)
+getDiveProfile(-18)
+getDiveProfile(-25)
+getDiveProfile(-30)
+getDiveProfile(-37)
 
