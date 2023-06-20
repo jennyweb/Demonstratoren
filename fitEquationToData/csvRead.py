2575014bad89819ddd919175e391cb16bba23143
Jennifer Weber <jennifer.weber@t-online.de>
2023-06-20 08:58:22 +0200

    changed list names

--- fitEquationToData/csvRead.py
+++ fitEquationToData/csvRead.py
@@ -7,20 +7,20 @@ currentWorkingDir = os.path.dirname(__file__)
 dataPath = os.path.join(currentWorkingDir, 'sizeDistribution.xlsx')
 
 df = pd.read_excel(dataPath, header=1)
-size = []
-volume = []
-columnC = []
+sizeDistribution = []
+volumeFrequency = []
+accumulatedVolumeFrequency = []
 for i in range(len(df['Größe (μm)'])):
-    size.append(df['Größe (μm)'][i])
-    volume.append(df['% Volumen In'][i])
-    columnC.append(df[df.columns[2]][i])
+    sizeDistribution.append(df['Größe (μm)'][i])
+    volumeFrequency.append(df['% Volumen In'][i])
+    accumulatedVolumeFrequency.append(df[df.columns[2]][i])
 
 
 sigma = 0.2
 µ = np.log(40)-0.5*sigma**2
 lognormDistribution = []
-for i in range(len(size)):
-    x = (1./(sigma * size[i] * np.sqrt(2* np.pi))) * np.exp(-((np.log(size[i])-µ)**2)/(2*sigma**2))
+for i in range(len(sizeDistribution)):
+    x = (1./(sigma * sizeDistribution[i] * np.sqrt(2* np.pi))) * np.exp(-((np.log(sizeDistribution[i])-µ)**2)/(2*sigma**2))
     lognormDistribution.append(x)
 
 lognormDistribution = np.array(lognormDistribution)
@@ -28,13 +28,13 @@ lognormDistribution *= 100./np.sum(lognormDistribution)
 
 imageCounter = 0
 
-for i in range(len(columnC)):
-    if columnC[i] == 100:
-        xmax = size[i +4]
+for i in range(len(accumulatedVolumeFrequency)):
+    if accumulatedVolumeFrequency[i] == 100:
+        xmax = sizeDistribution[i +4]
         break
 
-plt.plot(size, volume, color = 'blue', label = 'data')
-plt.plot(size, lognormDistribution, color ='green', label = 'lognormDistribution')
+plt.plot(sizeDistribution, volumeFrequency, color = 'blue', label = 'data')
+plt.plot(sizeDistribution, lognormDistribution, color ='green', label = 'lognormDistribution')
 plt.legend(loc='upper left')
 plt.xscale('log')
 plt.xlim([1,xmax])
