9eac401f419780df4d39e5e75d270e19b7ab6fc6
Jennifer Weber <jennifer.weber@t-online.de>
2023-06-20 09:00:31 +0200

    defined function

--- fitEquationToData/csvRead.py
+++ fitEquationToData/csvRead.py
@@ -15,34 +15,34 @@ for i in range(len(df['Größe (μm)'])):
     volumeFrequency.append(df['% Volumen In'][i])
     accumulatedVolumeFrequency.append(df[df.columns[2]][i])
 
+def getDeviation(µ):
+    sigma = 0.2
+    µ = np.log(40)-0.5*sigma**2
+    lognormDistribution = []
+    for i in range(len(sizeDistribution)):
+        x = (1./(sigma * sizeDistribution[i] * np.sqrt(2* np.pi))) * np.exp(-((np.log(sizeDistribution[i])-µ)**2)/(2*sigma**2))
+        lognormDistribution.append(x)
 
-sigma = 0.2
-µ = np.log(40)-0.5*sigma**2
-lognormDistribution = []
-for i in range(len(sizeDistribution)):
-    x = (1./(sigma * sizeDistribution[i] * np.sqrt(2* np.pi))) * np.exp(-((np.log(sizeDistribution[i])-µ)**2)/(2*sigma**2))
-    lognormDistribution.append(x)
+    lognormDistribution = np.array(lognormDistribution)
+    lognormDistribution *= 100./np.sum(lognormDistribution)
 
-lognormDistribution = np.array(lognormDistribution)
-lognormDistribution *= 100./np.sum(lognormDistribution)
+    imageCounter = 0
 
-imageCounter = 0
+    for i in range(len(accumulatedVolumeFrequency)):
+        if accumulatedVolumeFrequency[i] == 100:
+            xmax = sizeDistribution[i +4]
+            break
 
-for i in range(len(accumulatedVolumeFrequency)):
-    if accumulatedVolumeFrequency[i] == 100:
-        xmax = sizeDistribution[i +4]
-        break
-
-plt.plot(sizeDistribution, volumeFrequency, color = 'blue', label = 'data')
-plt.plot(sizeDistribution, lognormDistribution, color ='green', label = 'lognormDistribution')
-plt.legend(loc='upper left')
-plt.xscale('log')
-plt.xlim([1,xmax])
-plt.xlabel('size in µm')
-plt.ylabel('size distribution in %')
-plt.title(f'current sigma = {sigma}, current µ = {µ:03f}')
-plt.savefig(os.path.join(currentWorkingDir,f'Distribution-{imageCounter:04d}.png'))
-plt.close()
+    plt.plot(sizeDistribution, volumeFrequency, color = 'blue', label = 'data')
+    plt.plot(sizeDistribution, lognormDistribution, color ='green', label = 'lognormDistribution')
+    plt.legend(loc='upper left')
+    plt.xscale('log')
+    plt.xlim([1,xmax])
+    plt.xlabel('size in µm')
+    plt.ylabel('size distribution in %')
+    plt.title(f'current sigma = {sigma}, current µ = {µ:03f}')
+    plt.savefig(os.path.join(currentWorkingDir,f'Distribution-{imageCounter:04d}.png'))
+    plt.close()
 
 
 
