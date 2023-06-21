7bacb64f58ddee57419d09f3dd4683c4883e4e5d
Jennifer Weber <jennifer.weber@t-online.de>
2023-06-21 10:12:21 +0200

    created time and depth list for plotting

--- divingComputer/tauchprofil.py
+++ divingComputer/tauchprofil.py
@@ -1,45 +1,42 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
-def getDiveProfile(depth): 
-    currentDepth = 0
-    filename = f'divingprofile_{depth}'
-    with open(f'DivingComputer\{filename}.txt', 'w') as fout:
-        fout.write('time\tdepth\n')
-        for i in range(240):
-            currentTime = i * 10
-            if currentDepth > depth:
-                if currentTime % 3 == 0:
-                    currentDepth = currentDepth
-                elif currentTime % 4 == 0:
-                    currentDepth = currentDepth
-                else:
-                    currentDepth -= i * 0.3
-                    currentDepth = max(currentDepth, depth)
-                fout.write(f'{currentTime}\t{currentDepth}\n')
-            else: 
-                currentDepth = depth
-                fout.write(f'{currentTime}\t{currentDepth}\n')
-    time = []
-    depth = []
-    with open(f'DivingComputer\{filename}.txt', 'r') as fin:
-        for line in csv.DictReader(fin, delimiter='\t'):
-            time.append(line['time'])
-            depth.append(float(line['depth']))
-    visualizeDivingProfile(time,depth, filename)
+def getDiveProfile(x): 
+    depth = [0]
+    time = [0]
+    depthNp = np.random.randint(10,42, size=x).tolist()
+    timeNp = np.random.randint(5,15, size=x).tolist()
+    for i in range(len(depthNp)):
+        depth.append(depthNp[i])
+        depth.append(depthNp[i])
+        time.append(time[-1]+1)
+        time.append(timeNp[i])
+    print(depth,time)
+
+    # filename = f'divingprofile_{depth}'
+    # with open(f'DivingComputer\{filename}.txt', 'w') as fout:
+    #     fout.write('time\tdepth\n')
+     
+        #         currentDepth = depth
+        #         fout.write(f'{currentTime}\t{currentDepth}\n')
+    # time = []
+    # depth = []
+    # with open(f'DivingComputer\{filename}.txt', 'r') as fin:
+    #     for line in csv.DictReader(fin, delimiter='\t'):
+    #         time.append(line['time'])
+    #         depth.append(float(line['depth']))
+    # visualizeDivingProfile(time,depth, filename)
     
-def visualizeDivingProfile(time,depth,filename):    
-    plt.plot((time)/60, depth)
-    plt.title(filename)
-    plt.xlabel('time')
-    plt.ylabel('depth')
-    plt.savefig(f'DivingComputer\{filename}.png')
-    plt.close()
+# def visualizeDivingProfile(time,depth,filename):    
+#     plt.plot(time, depth)
+#     plt.title(filename)
+#     plt.xlabel('time')
+#     plt.ylabel('depth')
+#     plt.savefig(f'DivingComputer\{filename}.png')
+#     plt.close()
 
 
-getDiveProfile(-13)
-getDiveProfile(-18)
-getDiveProfile(-25)
-getDiveProfile(-30)
-getDiveProfile(-37)
+print(getDiveProfile(3))
+
 
