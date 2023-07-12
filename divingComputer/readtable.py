8eae3c46680d9355719794299f89620cf03d69c9
BastienDietemann <bastien.dietemann@iwm.fraunhofer.de>
2023-07-12 18:30:56 +0200

    fixed readtable at some minor positions

--- divingComputer/readtable.py
+++ divingComputer/readtable.py
@@ -5,91 +5,92 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
-np.random.seed(497)
+np.random.seed(492)
 
 
-def getKeyForDepth(depth):
-    if depth > keysInFindPressureGroup[0]:
-        raise RuntimeError (f'please make sure that the depth does not exceed 42m. I got {depth}')
+
+
+
         
-    for i in range(len(keysInFindPressureGroup)):
-        if abs(depth) >= keysInFindPressureGroup[i]:
-            key = keysInFindPressureGroup[i-1]
-            return key
-    return keysInFindPressureGroup[-1]
-
-def getPressureGroup(key,time):
-    pressureGroupforTt = None
-    minutesInCertainDepth = findPressureGroup[key]
-    for i in range(len(minutesInCertainDepth)):
-        if minutesInCertainDepth[i] >= time:
-            pressureGroupforTt = pressureGroup[i]
-            return pressureGroupforTt
-        
-def getmaxTime1stDive(key):
-    maxTimeDive1 = 0
-    for i in range(len(findPressureGroup[key])):
-        if  np.isnan(findPressureGroup[key][i]):
-            maxTimeDive1 = findPressureGroup[key][i-1]
-        return maxTimeDive1
+def getMaxTimeFirstDive(key):
+    i = 0
+    while not np.isnan(findPressureGroup[key][i]):
+        i += 1
+    return findPressureGroup[key][i-1]
         
 
 
 # visualization of dive profile and pressure groups
-def visualizeDivingProfile(time,depth,filename):    
-    fig, ax = plt.subplots(figsize=(9, 6))
-    ax.plot(time, depth, color = 'green',lw=2)
+def visualizeDivingProfile(time, depth, filename):    
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(time, depth, color = 'green', lw=2)
     plt.title(filename)
     plt.xlabel('time in min')
     plt.ylabel('depth in m')
-    ax.annotate(f'PG1 = {oldPG}', xy=(time[2], 0.25), color= 'blue')
-    ax.annotate(f'PG2 = {currentPG}', xy=(time[4]-3,0.25), color= 'blue')
+    ax.annotate(f'PG1 = {pressureGroupAfterFirstDive}', xy=(time[2], 0.25), color= 'blue')
+    ax.annotate(f'PG2 = {pressureGroupAfterSurfaceInterval}', xy=(time[4]-3,0.25), color= 'blue')
     ax.annotate(f'PG3 = {PG3}', xy=(time[6]-3,0.25), color= 'blue')
-    ax.annotate(f'BT = {depthAndTime[0][1]} min', xy=(time[1]+1,depth[1]+0.25), color= 'orange')
-    ax.annotate(f'ST = {depthAndTime[1][1]} min', xy=(time[4]/2,depth[3]+0.25), color= 'orange')
+    ax.annotate(f'BT = {dataForDiveComputerDepthTime[0][1]} min', xy=(time[1]+1,depth[1]+0.25), color= 'orange')
+    ax.annotate(f'ST = {dataForDiveComputerDepthTime[1][1]} min', xy=(time[4]/2,depth[3]-0.5), color= 'orange')
     ax.annotate(f'BT = {int(maximumBottomTime)} min', xy=(time[5]+1,depth[5]+0.25), color= 'orange')
     plt.savefig(os.path.join(currentWorkingDir, f'{filename}.png'))
     plt.close()
 
 
-# saving depth and time to .txt file
-def writeDataForDivingComputer(dataForDiveComputerDepthTime,filename):
-    with open(os.path.join(currentWorkingDir, f'{filename}.txt'),'w') as fout:
-        fout.write('depth time\n')
-        for i in range(len(dataForDiveComputerDepthTime)):
-            fout.write(f'{dataForDiveComputerDepthTime[i]}\n')
 
 
-def getPressureGrAfterSurfaceIntervall(oldPG, surfaceTime):
-    adequateTimeIntervall = 0
-    timezones = list(mappingSurfaceTimeToNewPgroup[oldPG].keys())
-    timezones.sort()
-    for i in range(len(timezones)):
-        if surfaceTime < timezones[i]:
-            adequateTimeIntervall = timezones[i-1]
-            break
-    return mappingSurfaceTimeToNewPgroup[oldPG][adequateTimeIntervall]
 
 
-def getMaxBottomTime(currenPG, desiredDivingDepth):
-    desiredDivingDepthGroup = None
-    maximumBottomTime = 0
-    for i in range(len(depthsfor2ndDive)):
-        if abs(desiredDivingDepth[0]) < depthsfor2ndDive[i]:
-            desiredDivingDepthGroup = depthsfor2ndDive[i]
-            break
-    maximumBottomTime = maxBottomTime[currenPG][desiredDivingDepthGroup]
-    resN2Time = residualNitrogenTime[currenPG][desiredDivingDepthGroup]
-    time.append(int(time[-1]+maximumBottomTime))
+def createDepthProfile(depthNp, depthSecondDive):
+    depth = [0]
+    # add bottom time
+    for _ in range(2):
+        depth.append(depthNp)
+
+    # add surface time
+    for _ in range(2):
+        depth.append(0)
+
+    # add second bottom time
+    for _ in range(2):
+        depth.append(depthSecondDive)
+
+    # go to surface again
+    depth.append(0)
+    return depth
+
+
+def createTimeProfile(timeNp, surfaceTime):
+
+    # start at surface
+    time = [0]
+
+    # estimate that changes in depths happen within one minute
     time.append(time[-1]+1)
-    return maximumBottomTime, time, resN2Time
+    time.append(timeNp+time[-1])
 
-def getPressureGroupAfter2ndDive(maximumBottomTime, resN2Time):
-    totalBottomTime = maximumBottomTime + resN2Time
-    key2ndDive = getKeyForDepth(desiredDepth2ndDive[0])
-    PG3 = getPressureGroup(key2ndDive,totalBottomTime)
-    return PG3
+    time.append(time[-1]+1)
+    time.append(time[-1]+surfaceTime)
+    time.append(time[-1]+1)
 
+    return time
+
+
+def combineProfiles(depthProfile, timeProfile):
+    dataForDiveComputerDepthTime = []
+    for i in range(2,len(timeProfile)):
+        if i % 2 == 0:
+            dataForDiveComputerDepthTime.append((depthProfile[i],(timeProfile[i]-timeProfile[i-1])))
+    return dataForDiveComputerDepthTime
+
+
+def getSurfaceTimeAsDatetimeObject(surfaceTime):
+    if surfaceTime > 59:
+        hour, minutes = divmod(surfaceTime, 60)
+    else:
+        hour = 0
+        minutes = surfaceTime
+    return datetime.time(hour,minutes)
 
 
 ########################################################################
@@ -119,6 +120,27 @@ for i,column_name in enumerate(dfFindPressureGroup): #iterates through depths, i
 keysInFindPressureGroup.sort(reverse=True) 
 
 
+def getKeyForDepth(depth):
+    if depth > keysInFindPressureGroup[0]:
+        raise RuntimeError (f'please make sure that the depth does not exceed 42m. I got {depth}')
+        
+    for i in range(len(keysInFindPressureGroup)):
+        if abs(depth) >= keysInFindPressureGroup[i]:
+            key = keysInFindPressureGroup[i-1]
+            return key
+    return keysInFindPressureGroup[-1]
+
+assert getKeyForDepth(-30) >= 30., 'key extraction failed'
+
+
+def getPressureGroup(key,time):
+    minutesInCertainDepth = findPressureGroup[key]
+    for i in range(len(minutesInCertainDepth)):
+        if minutesInCertainDepth[i] >= time:
+            return pressureGroup[i]
+
+assert getPressureGroup(30.48,17) == 'L', 'Mapping of bottom time to pressure group failed'
+
 
 
 ########################################################################
@@ -146,6 +168,17 @@ for i,column_name in enumerate(pressureGroupAfterSurfaceInterval):
             mappingSurfaceTimeToNewPgroup[indexToPressureGroup[j]][pressureGroupAfterSurfaceInterval[column_name].iloc[j]] = column_name
 
 
+def getPressureGroupAfterSurfaceInterval(oldPG, surfaceTime, mappingSurfaceTimeToNewPgroup):
+    adequateTimeInterval = 0
+    timezones = list(mappingSurfaceTimeToNewPgroup[oldPG].keys())
+    timezones.sort()
+    for i in range(len(timezones)):
+        if surfaceTime < timezones[i]:
+            adequateTimeInterval = timezones[i-1]
+            break
+    return mappingSurfaceTimeToNewPgroup[oldPG][adequateTimeInterval]
+
+assert getPressureGroupAfterSurfaceInterval('L', getSurfaceTimeAsDatetimeObject(42), mappingSurfaceTimeToNewPgroup) == 'F', 'Mapping of initial pressure group to new pressure group after surface interval failed'
 
 
 
@@ -156,106 +189,88 @@ for i,column_name in enumerate(pressureGroupAfterSurfaceInterval):
 dfBottomTimeSecondDive = pd.read_excel(dataPath, sheet_name='max bottom time 2nd dive')
 residualNitrogenTime = {}
 maxBottomTime = {}
-oldPressureGroupInBottomTime = []
-depthsfor2ndDive = []
-indexBottomTime = {}
-indexResNitrogen = {}
-for i,keyPG in enumerate(dfBottomTimeSecondDive): # iterate through Z Y X W ...
+depthsForSecondDive = []
+for i, keyPG in enumerate(dfBottomTimeSecondDive): # iterate through Z Y X W ...
     if i > 0: # first column contains the depths and it is not a key of interest
-        oldPressureGroupInBottomTime.append(keyPG)
-        maxBottomTime[keyPG] = {}
-        residualNitrogenTime[keyPG] = {}
-for i,depth2ndDive in enumerate(dfBottomTimeSecondDive['depth in m pressure group at the end of surface intervall']):
+        maxBottomTime[keyPG] = {} # initiate with empty dictionary 
+        residualNitrogenTime[keyPG] = {} # initiate with empty dictionary
+
+for i, depth2ndDive in enumerate(dfBottomTimeSecondDive['depth in m pressure group at the end of surface intervall']): # iterate over first column
     if not pd.isnull(depth2ndDive):
-        depth2ndDive = float(f'{float(depth2ndDive):1.02f}')
-        depthsfor2ndDive.append(depth2ndDive)
-        indexBottomTime[i+1] =depth2ndDive
-        indexResNitrogen[i] = depth2ndDive
-depthsfor2ndDive.sort()
+        depth2ndDive = float(f'{float(depth2ndDive):1.02f}') # round 
+        depthsForSecondDive.append(depth2ndDive)
+
+depthsForSecondDive.sort()
 for i, column_name in enumerate(dfBottomTimeSecondDive):
     if i == 0:
         continue
-    counter = 1
-    counterN2 = 0
-    for j in range(len(depthsfor2ndDive)):
-            
-            if not pd.isnull(dfBottomTimeSecondDive[column_name].iloc[j]):
-                maxBottomTime[column_name][depthsfor2ndDive[j]] = dfBottomTimeSecondDive[column_name].iloc[counter]
-            counter += 2
-            if not pd.isnull(dfBottomTimeSecondDive[column_name].iloc[j]):
-                residualNitrogenTime[column_name][depthsfor2ndDive[j]] = dfBottomTimeSecondDive[column_name].iloc[counterN2]
-            counterN2 += 2
+
+    for j in range(0,len(depthsForSecondDive)):
+        if not pd.isnull(dfBottomTimeSecondDive[column_name].iloc[j*2]):
+            residualNitrogenTime[column_name][depthsForSecondDive[j]] = dfBottomTimeSecondDive[column_name].iloc[j*2]
+        if not pd.isnull(dfBottomTimeSecondDive[column_name].iloc[j*2+1]):
+            maxBottomTime[column_name][depthsForSecondDive[j]] = dfBottomTimeSecondDive[column_name].iloc[j*2+1]
+
+
+def updateTimeProfile(timeProfile, maximumBottomTime):
+    timeProfile.append(int(timeProfile[-1]+maximumBottomTime))
+    timeProfile.append(timeProfile[-1]+1)
+
+
+def getMaxBottomTimeAndMaximumNitrogenResidual(currentPressureGroup, desiredDivingDepth):
+    for i in range(len(depthsForSecondDive)):
+        if abs(desiredDivingDepth) < depthsForSecondDive[i]:
+            key = depthsForSecondDive[i]
+            break
+    maximumBottomTime = maxBottomTime[currentPressureGroup][key]
+    resN2Time = residualNitrogenTime[currentPressureGroup][key]
+    return maximumBottomTime, resN2Time
+
+
+assert getMaxBottomTimeAndMaximumNitrogenResidual('N', 25) == (3.0, 22.0), 'Extraction of maximum bottom time and residual nitrogen failed'
+
+
+def getPressureGroupAfter2ndDive(maximumBottomTime, resN2Time):
+    totalBottomTime = maximumBottomTime + resN2Time
+    key2ndDive = getKeyForDepth(depthSecondDive)
+    PG3 = getPressureGroup(key2ndDive,totalBottomTime)
+    return PG3
 
 
 ##############################
 # creating random dive profile
 ##############################
 
-# start at surface
-depth = [0]
-time = [0]
-
 # choose first bottom time
-depthNp = np.random.randint(-42,-10, 1).tolist()
-# limit maximum time at bottom to whatever is allowed according to the table
+depthFirstDive = np.random.randint(-42,-10, 1).tolist()[0] # yields -30
 
-key = getKeyForDepth(depthNp[0])
-timeNp = np.random.randint(3,getmaxTime1stDive(key), 1).tolist()
-surfaceTime = np.random.randint(10,70,1).tolist()
-maxDepthSecondDive = depthNp[0]
+# limit maximum time at bottom to whatever is allowed according to the table
+key = getKeyForDepth(depthFirstDive)
+maxTimeFirstDive = int(getMaxTimeFirstDive(key))
+bottomTimeFirstDive = np.random.randint(3,maxTimeFirstDive, 1).tolist()[0]
+surfaceTime = np.random.randint(10,70,1).tolist()[0]
+maxDepthSecondDive = depthFirstDive
 
 # limit first bottom time to maximum first depth. 
-depthSecondDive = np.random.randint(maxDepthSecondDive,-10, 1).tolist()
-
-# add bottom time
-for i in range(2):
-    depth.append(depthNp[0])
-
-# add surface time
-for i in range(2):
-    depth.append(0)
-
-# add second bottom time
-for i in range(2):
-    depth.append(depthSecondDive[0])
-
-# go to surface again
-depth.append(0)
-
-# estimate that changes in depths happen within one minute
-for i in range(len(timeNp)):
-    time.append(time[-1]+1)
-    time.append(timeNp[0]+time[-1])
-time.append(time[-1]+1)
-time.append(time[-1] +surfaceTime[0])
-time.append(time[-1]+1)
-
-dataForDiveComputerDepthTime = []
-for i in range(2,len(time)):
-    if i % 2 == 0:
-        dataForDiveComputerDepthTime.append((depth[i],(time[i]-time[i-1])))
-
-depthAndTime = dataForDiveComputerDepthTime
-desiredDepth2ndDive = depthSecondDive
+depthSecondDive = np.random.randint(maxDepthSecondDive,-10, 1).tolist()[0]
 
 
-key = getKeyForDepth(depthAndTime[0][0])
-oldPG = getPressureGroup(key,depthAndTime[0][1])
-if depthAndTime[1][1] > 59:
-    hour, minutes= divmod(depthAndTime[1][1], 60)
-else:
-    hour = 0
-    minutes = depthAndTime[1][1]
-
-surfaceTime = datetime.time(hour,minutes)
+depthProfile = createDepthProfile(depthFirstDive, depthSecondDive)
+timeProfile = createTimeProfile(bottomTimeFirstDive, surfaceTime)
+dataForDiveComputerDepthTime = combineProfiles(depthProfile, timeProfile)
 
 
 
+##############################
+# derive the pressure groups
+##############################
 
-currentPG = getPressureGrAfterSurfaceIntervall(oldPG, surfaceTime)
-maximumBottomTime, time, resN2Time = getMaxBottomTime(currentPG, desiredDepth2ndDive)
+pressureGroupAfterFirstDive = getPressureGroup(key,dataForDiveComputerDepthTime[0][1])
+surfaceTimeDatetimeObject = getSurfaceTimeAsDatetimeObject(dataForDiveComputerDepthTime[1][1])
+pressureGroupAfterSurfaceInterval = getPressureGroupAfterSurfaceInterval(pressureGroupAfterFirstDive, surfaceTimeDatetimeObject, mappingSurfaceTimeToNewPgroup)
+
+maximumBottomTime, resN2Time = getMaxBottomTimeAndMaximumNitrogenResidual(pressureGroupAfterSurfaceInterval, depthSecondDive)
+updateTimeProfile(timeProfile,maximumBottomTime)
 PG3 = getPressureGroupAfter2ndDive(maximumBottomTime, resN2Time)
-filename = f'divingprofile_{depthAndTime[0][0]}'
-visualizeDivingProfile(time,depth, filename)
-writeDataForDivingComputer(depthAndTime, filename)
+visualizeDivingProfile(timeProfile, depthProfile, 'divingProfile')
 
